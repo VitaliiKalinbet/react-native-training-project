bb2dea23384efeee6165419fb694c3bf295fe69b
Vitalii Kalinbet <vitalii.kalinbet@patrianna.com>
2025-11-16 13:21:19 +0100

    add pressed styles when remove item

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,22 +2,28 @@ import { Text, View, StyleSheet, Pressable } from 'react-native';
 
 export default function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable
+        android_ripple={{ color: '#210644' }}
+        style={({ pressed }) => pressed && styles.pressedItem}
+        onPress={props.onDeleteItem.bind(this, props.id)}>
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   )
 };
 
 const styles = StyleSheet.create({
   goalItem: {
     margin: 8,
-    padding: 8,
     borderRadius: 6,
     backgroundColor: '#5e0acc',
   },
+  pressedItem: {
+    opacity: 0.5,
+  },
   goalText: {
     color: 'white',
+    padding: 8,
   },
 });
